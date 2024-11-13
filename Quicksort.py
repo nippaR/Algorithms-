e132d79600366b5366bf808517c11a488dea2514
Thanuja Nipun <thanujanipun6@gmail.com>
2024-11-14 01:00:14 +0530

    Add QuickSort Algorithom

---
+++ Quicksort.py
@@ -0,0 +1,31 @@
+def quicksort(arr,left,right):
+    if left < right:
+        partition_pos = partition(arr,left,right)
+        quicksort(arr,left,partition_pos-1)
+        quicksort(arr,partition_pos+1,right)
+
+
+def partition(arr,left,right):
+    i = left
+    j = right - 1
+    pivot = arr[right]
+
+    while i < j:
+        while i < right and arr[i] < pivot:
+            i += 1
+        while j > left and arr[j] >= pivot:
+            j -= 1
+        if i < j:
+            arr[i],arr[j] = arr[j],arr[i]
+        
+    if arr[i] > pivot:
+        arr[i],arr[right] = arr[right],arr[i]
+    return i
+
+arr = []
+
+for i in range(0,5):
+    arr.append(int(input("Enter the number: ")))
+
+quicksort(arr,0,len(arr)-1)
+print(arr)
